72aac002443e041b30bfc0541c425669f1425751
Katja <ekaterinas.scher@gmail.com>
2020-06-14 13:44:40 +0200

    10. TinkoffTest update

--- src/test/java/tests/TinkoffTest.java
+++ src/test/java/tests/TinkoffTest.java
@@ -14,70 +14,79 @@ import static io.qameta.allure.Allure.parameter;
 import static io.qameta.allure.Allure.step;
 
 @Feature("Test TinkoffBankPage")
-    @Tag("tinkoff")
+@Tag("tinkoff")
 
-public class TinkoffTest extends TestBase{
+  public class TinkoffTest extends TestBase{
   private static String CURRENCY = "Доллары США";
   private static String AMOUNT  = "5000";
 
 
     @Test
     @Story("TinkoffBank test")
-    @DisplayName("Positive test")
+    @DisplayName("Positive test with calculator form")
 
-        void successfulTestTinkoff(){
-        parameter("Валюта:",CURRENCY);
-        parameter("Сумма:",AMOUNT);
+    void successfulTestTinkoff() {
+        parameter("Валюта:", CURRENCY);
+        parameter("Сумма:", AMOUNT);
 
-        //arrange
-        step("Открываем главную страницу",()->{
+        step("Открываем главную страницу", () -> {
             open("https://www.tinkoff.ru/");
         });
-        //act
-        step("Кликаем на 'Вклады'",()->{
+        step("Кликаем на 'Вклады'", () -> {
             $(byText("Вклады")).closest("a").click();
         });
         //для заморозки dom используем в консоли команду setTimeout(function(){debugger},5000)
-        step("В drop-down 'Валюта 'выбираем валюту $",()->{
-            $("[data-qa-type='uikit/inputBox']",2).click();
+        step("В drop-down 'Валюта 'выбираем валюту $", () -> {
+            $("[data-qa-type='uikit/inputBox']", 2).click();
             $("[data-qa-type='uikit/scroll']").$(byText(CURRENCY)).click();
         });
-        //assert
-        step("Проверяем,что валюта установилась корректно",()->{
-            $("[data-qa-type='uikit/inputBox']",2).$("[data-qa-type='uikit/select.value']")
+        step("Проверяем,что валюта установилась корректно", () -> {
+            $("[data-qa-type='uikit/inputBox']", 2).$("[data-qa-type='uikit/select.value']")
                     .shouldHave((text(CURRENCY)));
         });
-        //act
-        step("Устанавливаем сумму вклада 5000",()->{
+        step("Устанавливаем сумму вклада 5000", () -> {
             $("[data-qa-type='uikit/inputBox.inputContainer'] input").setValue(AMOUNT);
         });
-        //assert
-        step("Проверяем, что сумма устанавилась корректно",()->{
+        step("Проверяем, что сумма устанавилась корректно", () -> {
             $("[data-qa-type='uikit/inputBox.inputContainer'] input").shouldHave(value(AMOUNT));
         });
-        //assert
-        step("Проверяем наличие hover с нужным текстом",()->{
-            $("div[data-qa-file=Pie]").scrollIntoView(true);
-            $("div[data-qa-file=Pie]").shouldHave(text("за 24 месяца я накоплю"));
+}
+     @Test
+     @Story("TinkoffBAnk Website, Hover on Page")
+     @DisplayName("Positive Test,the hover shows the expected text")
+
+     void successfulTestWithHover() {
+         step("Открываем страницу с калькулятором вклада", () -> {
+             open("https://www.tinkoff.ru/deposit/");
+         });
+         step("Проверяем наличие hover с нужным текстом", () -> {
+             $("div[data-qa-file=Pie]").scrollIntoView(true);
+             $("div[data-qa-file=Pie]").shouldHave(text("за 24 месяца я накоплю"));
+         });
+         step("Наводим курсор на hover", () -> {
+             $("div[data-qa-file=Pie]").hover();
+         });
+         step("Проверяем, что текст изменился на ожидаемый", () -> {
+             $("div[data-qa-file=Pie]").shouldHave(text("пополнения"));
+         });
+     }
+
+      @Test
+      @Story("TinkoffBAnk Website, Checkbox on Page")
+      @DisplayName("Positive Test,remove the flag in the checkbox")
+
+      void successfulTestWithCheckbox(){
+        step("Открываем страницу с калькулятором вклада",()->{
+            open("https://www.tinkoff.ru/deposit/");
         });
-        //act
-        step("Наводим курсор на hover",()->{
-            $("div[data-qa-file=Pie]").hover();
-        });
-        //assert
-        step("Проверяем, что текст изменился на ожидаемый",()->{
-            $("div[data-qa-file=Pie]").shouldHave(text("пополнения"));
-        });
-        //act
         step("Снимаем флаги в checkbox",()->{
+            $("div[data-qa-file=Pie]").scrollIntoView(true);
             $("[data-qa-type='uikit/checkbox']").click();
+        });
+        step("Проверяем, что флаг снят",()->{
             $("[aria-checked]").shouldNotBe(checked);
 
         });
 
-    }
-
-
-
-
+         }
 }
